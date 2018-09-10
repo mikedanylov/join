89aad49fc280562e3a8732f98b9a43319bee9796
mikedanylov <mike.danylov@gmail.com>
2018-09-10 12:03:42 +0300

    feat: Add id property to objects

--- join.spec.js
+++ join.spec.js
@@ -1,24 +1,26 @@
-var expect = require('chai').expect;
-var join = require('./join');
+const expect = require('chai').expect;
+const uuidv4 = require('uuid/v4');
+const join = require('./join');
 
-describe('join', function() {
-  it('should accept no arguments', function() {
-    var result = join();
-
-    expect(result).to.be.instanceof(Array);
-    expect(result.length).to.be.equal(0);
-  });
-
-  it('should accept single empty array', function() {
-    var result = join([]);
+describe('join', () => {
+  it('should accept no arguments', () => {
+    const result = join();
 
     expect(result).to.be.instanceof(Array);
     expect(result.length).to.be.equal(0);
   });
 
-  it('should accept single non-empty array', function() {
-    var result = join([
+  it('should accept single empty array', () => {
+    const result = join([]);
+
+    expect(result).to.be.instanceof(Array);
+    expect(result.length).to.be.equal(0);
+  });
+
+  it('should accept single non-empty array', () => {
+    const result = join([
       {
+        id: uuidv4(),
         lastModified: new Date('2018-08-08T07:00:00.000Z')
       }
     ]);
@@ -27,19 +29,22 @@ describe('join', function() {
     expect(result.length).to.be.equal(1);
   });
 
-  it('should accept multiple arrays', function() {
-    var result = join(
+  it('should accept multiple arrays', () => {
+    const result = join(
       [],
       [
         {
+          id: uuidv4(),
           lastModified: new Date('2018-08-08T07:00:00.000Z')
         }
       ],
       [
         {
+          id: uuidv4(),
           lastModified: new Date('2018-08-09T07:00:00.000Z')
         },
         {
+          id: uuidv4(),
           lastModified: new Date('2018-08-10T07:00:00.000Z')
         }
       ]
@@ -49,35 +54,31 @@ describe('join', function() {
     expect(result.length).to.be.equal(3);
   });
 
-  it('should filter out duplicates', function() {
-    var duplicate = '2018-08-08T07:00:00.000Z';
-    var result = join(
+  it('should filter out duplicates', () => {
+    const duplicate = {
+      id: uuidv4(),
+      lastModified: new Date('2018-08-08T07:00:00.000Z')
+    };
+
+    const result = join(
       [
-        {
-          lastModified: new Date(duplicate)
-        }
+        Object.assign({}, duplicate)
       ],
       [
         {
           lastModified: new Date('2018-08-09T07:00:00.000Z')
         },
-        {
-          lastModified: new Date(duplicate)
-        }
+        Object.assign({}, duplicate)
       ],
       [
         {
           lastModified: new Date('2018-08-10T07:00:00.000Z')
         },
-        {
-          lastModified: new Date(duplicate)
-        },
+        Object.assign({}, duplicate),
         {
           lastModified: new Date('2018-08-11T07:00:00.000Z')
         },
-        {
-          lastModified: new Date(duplicate)
-        }
+        Object.assign({}, duplicate)
       ]
     );
 
@@ -85,48 +86,45 @@ describe('join', function() {
     expect(result.length).to.be.equal(4);
   });
 
-  it('should sort by lastModified property in ascending order', function() {
-    var duplicate = '2018-08-08T07:00:00.000Z';
-    var first = duplicate;
-    var second = '2018-08-09T07:00:00.000Z';
-    var third = '2018-08-10T07:00:00.000Z';
-    var fourth = '2018-08-11T07:00:00.000Z';
-    var result = join(
+  it('should sort by lastModified property in ascending order', () => {
+    const first = {
+      id: uuidv4(),
+      lastModified: new Date('2018-08-08T07:00:00.000Z')
+    };
+    const second = {
+      id: uuidv4(),
+      lastModified: new Date('2018-08-09T07:00:00.000Z')
+    };
+    const third = {
+      id: uuidv4(),
+      lastModified: new Date('2018-08-10T07:00:00.000Z')
+    };
+    const fourth = {
+      id: uuidv4(),
+      lastModified: new Date('2018-08-11T07:00:00.000Z')
+    };
+    const result = join(
       [],
       [
-        {
-          lastModified: new Date(third)
-        },
-        {
-          lastModified: new Date(duplicate)
-        },
-        {
-          lastModified: new Date(fourth)
-        },
-        {
-          lastModified: new Date(duplicate)
-        }
+        Object.assign({}, third),
+        Object.assign({}, first),
+        Object.assign({}, fourth),
+        Object.assign({}, first)
       ],
       [
-        {
-          lastModified: new Date(duplicate)
-        }
+        Object.assign({}, first)
       ],
       [
-        {
-          lastModified: new Date(second)
-        },
-        {
-          lastModified: new Date(duplicate)
-        }
+        Object.assign({}, second),
+        Object.assign({}, first)
       ]
     );
 
     expect(result).to.be.instanceof(Array);
     expect(result.length).to.be.equal(4);
-    expect(result[0].lastModified.toISOString()).to.be.equal(first);
-    expect(result[1].lastModified.toISOString()).to.be.equal(second);
-    expect(result[2].lastModified.toISOString()).to.be.equal(third);
-    expect(result[3].lastModified.toISOString()).to.be.equal(fourth);
+    expect(result[0].lastModified.toISOString()).to.be.equal(first.lastModified.toISOString());
+    expect(result[1].lastModified.toISOString()).to.be.equal(second.lastModified.toISOString());
+    expect(result[2].lastModified.toISOString()).to.be.equal(third.lastModified.toISOString());
+    expect(result[3].lastModified.toISOString()).to.be.equal(fourth.lastModified.toISOString());
   });
 });
